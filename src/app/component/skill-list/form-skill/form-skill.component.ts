bc673906c1b2ef5f33e2fe9c48a6b4ae40a21b59
AgustinBosch <agustinbosch@gmail.com>
2022-04-02 16:19:15 -0300

    algo de css

--- src/app/component/skill-list/form-skill/form-skill.component.ts
+++ src/app/component/skill-list/form-skill/form-skill.component.ts
@@ -3,52 +3,55 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Skill } from "src/app/model/skill.model";
 
 @Component({
-  selector: "app-form-skill",
-  templateUrl: "./form-skill.component.html",
-  styleUrls: ["./form-skill.component.css"],
+	selector: "app-form-skill",
+	templateUrl: "./form-skill.component.html",
+	styleUrls: ["./form-skill.component.css"],
 })
 export class FormSkillComponent {
-  mostrarForm: boolean = false;
-  @Output() nuevaSkill: EventEmitter<Skill> = new EventEmitter();
+	mostrarForm: boolean = false;
+	@Output() nuevaSkill: EventEmitter<Skill> = new EventEmitter();
 
-  form: FormGroup;
+	form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({
-      id: [0],
-      nombre: [
-        "",
-        [
-          Validators.required,
-          Validators.minLength(3),
-          Validators.maxLength(10),
-        ],
-      ],
-      nivel: [0, [Validators.required, Validators.min(1), Validators.max(10)]],
-    });
-  }
+	constructor(private formBuilder: FormBuilder) {
+		this.form = this.formBuilder.group({
+			id: [0],
+			nombre: [
+				"",
+				[
+					Validators.required,
+					Validators.minLength(3),
+					Validators.maxLength(10),
+				],
+			],
+			nivel: [
+				"",
+				[Validators.required, Validators.min(1), Validators.max(10)],
+			],
+		});
+	}
 
-  setSkill(skill: Skill) {
-    this.form.patchValue({
-      id: skill.id,
-      nombre: skill.nombre,
-      nivel: skill.nivel,
-    });
-    this.mostrarForm = true;
-  }
+	setSkill(skill: Skill) {
+		this.form.patchValue({
+			id: skill.id,
+			nombre: skill.nombre,
+			nivel: skill.nivel,
+		});
+		this.mostrarForm = true;
+	}
 
-  toggleForm() {
-    this.mostrarForm = !this.mostrarForm;
-  }
+	toggleForm() {
+		this.mostrarForm = !this.mostrarForm;
+	}
 
-  onSubmit(event: Event) {
-    event.preventDefault;
-    if (this.form.valid) {
-      this.nuevaSkill.emit(this.form.value);
-      this.form.reset();
-      this.mostrarForm = false;
-    } else {
-      console.log("form no valido");
-    }
-  }
+	onSubmit(event: Event) {
+		event.preventDefault;
+		if (this.form.valid) {
+			this.nuevaSkill.emit(this.form.value);
+			this.form.reset();
+			this.mostrarForm = false;
+		} else {
+			console.log("form no valido");
+		}
+	}
 }
--- src/app/component/skill-list/skill-list.component.ts
+++ src/app/component/skill-list/skill-list.component.ts
@@ -1,58 +1,54 @@
 import {
-  AfterViewInit,
-  Component,
-  Input,
-  OnInit,
-  ViewChild,
-  ViewChildren,
+	AfterViewInit,
+	Component,
+	Input,
+	OnInit,
+	ViewChild,
+	ViewChildren,
 } from "@angular/core";
 import { Skill } from "src/app/model/skill.model";
 import { SkillService } from "src/app/service/skill.service";
 import { FormSkillComponent } from "./form-skill/form-skill.component";
 
 @Component({
-  selector: "app-skill-list",
-  templateUrl: "./skill-list.component.html",
-  styleUrls: ["./skill-list.component.css"],
+	selector: "app-skill-list",
+	templateUrl: "./skill-list.component.html",
+	styleUrls: ["./skill-list.component.css"],
 })
 export class SkillListComponent {
-  @ViewChild("formSkill") formSkill!: FormSkillComponent;
-  @Input() skills: Skill[] = [];
+	@ViewChild("formSkill") formSkill!: FormSkillComponent;
+	@Input() skills: Skill[] = [];
 
-  constructor(private skillSevice: SkillService) {}
+	constructor(private skillSevice: SkillService) {}
 
-  crearNuevaSkill(skill: Skill) {
-    if (skill.id) {
-      this.skillSevice.editarSkill(skill).subscribe((skillEditada) => {
-        this.actualizarPrimeraSkill(skillEditada);
-      });
-    } else {
-      this.skillSevice
-        .nuevaSkill(skill)
-        .subscribe((nuevaskill) => this.skills.push(nuevaskill));
-    }
-  }
+	crearNuevaSkill(skill: Skill) {
+		if (skill.id) {
+			this.skillSevice.editarSkill(skill).subscribe((skillEditada) => {
+				this.actualizarPrimeraSkill(skillEditada);
+			});
+		} else {
+			this.skillSevice
+				.nuevaSkill(skill)
+				.subscribe((nuevaskill) => this.skills.push(nuevaskill));
+		}
+	}
 
-  borrarSkill(id: number) {
-    this.skillSevice.borrarSkill(id).subscribe(() => {
-      this.skills = this.skills.filter((t) => t.id !== id);
-    });
-  }
+	borrarSkill(id: number) {
+		this.skillSevice.borrarSkill(id).subscribe(() => {
+			this.skills = this.skills.filter((t) => t.id !== id);
+		});
+	}
 
-  private actualizarPrimeraSkill(skill: Skill) {
-    for (let index = 0; index < this.skills.length; index++) {
-      if (this.skills[index].id === skill.id) {
-        this.skills[index] = skill;
-        break;
-      }
-    }
-  }
+	private actualizarPrimeraSkill(skill: Skill) {
+		for (let index = 0; index < this.skills.length; index++) {
+			if (this.skills[index].id === skill.id) {
+				this.skills[index] = skill;
+				break;
+			}
+		}
+	}
 
-  toggleForm() {
-    this.formSkill.toggleForm();
-  }
-
-  editarSkill(skill: Skill) {
-    this.formSkill.setSkill(skill);
-  }
+	editarSkill(skill: Skill) {
+		this.formSkill.setSkill(skill);
+	}
 }
